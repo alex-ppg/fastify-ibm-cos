b6c09a333449123333bf76c27b738f1ee0a3a069
alex--pg <alex.ppg@protonmail.com>
2019-09-29 18:31:10 +0300

    Minor - Renamed plugin function

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const fastifyPlugin = require("fastify-plugin");
 const AWS = require("ibm-cos-sdk");
 
-async function mongooseConnector(fastify, config) {
+async function ibmConnector(fastify, config) {
   fastify.decorate("cos", new AWS.S3(config));
 }
 
-module.exports = fastifyPlugin(mongooseConnector);
+module.exports = fastifyPlugin(ibmConnector);
